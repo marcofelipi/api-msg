494133cb64cc518c01be0a6f88b4983f5a9bf8c9
marcofelipi <marco.felipi@hotmail.com>
2025-01-06 23:27:45 -0300

    fix: caminhos

--- server.js
+++ server.js
@@ -1,35 +1,23 @@
 const express = require('express');
-const nodemailer = require('nodemailer');
-const cors = require('cors');
 const app = express();
+const cors = require('cors');
+const nodemailer = require('nodemailer');
 const port = 3000;
 
-const cors = require('cors');
-
-app.use(cors({
-    origin: '*', // Permite requisições de qualquer origem. Altere para um domínio específico em produção, se necessário.
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Permite os métodos HTTP que você deseja aceitar
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'], // Permite cabeçalhos usados em requisições
-    exposedHeaders: ['Authorization'], // Permite que o cliente acesse cabeçalhos específicos na resposta
-    credentials: true, // Permite o envio de cookies ou credenciais de autenticação
-    optionsSuccessStatus: 204 // Status para respostas de OPTIONS (usado por alguns navegadores)
-}));
-
-
-// Middleware para parsing de JSON
 app.use(express.json());
 
-// Rota de teste
-app.get('/api/hello', (req, res) => {
-    res.json({ message: 'Hello from the API!' });
-});
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
 
-// Rota para envio de contato com e-mail
-app.post('/api/contact', async (req, res) => {
+app.post('/api/contact', (req, res) => {
     const { nome, email, mensagem } = req.body;
 
     if (!nome || !email || !mensagem) {
-        return res.status(400).json({ error: 'Preencha todos os campos obrigatórios!' });
+        return res.status(400).json({ error: 'Preencha todos os campos.' });
     }
 
     const transporter = nodemailer.createTransport({
@@ -43,12 +31,12 @@ app.post('/api/contact', async (req, res) => {
     const mailOptions = {
         from: 'no.reply.982289@gmail.com',
         to: 'marco@fotosim.com',
-        subject: `Novo contato: ${nome}`,
-        text: `Nome: ${nome}\nEmail: ${email}\nMensagem:\n${mensagem}`,
+        subject: `Novo contato em Gondolize: ${nome}`,
+        text: `Nome: ${nome}\nEmail: ${email}\nMensagem: ${mensagem}`,
     };
 
     try {
-        await transporter.sendMail(mailOptions);
+        transporter.sendMail(mailOptions);
         res.status(200).json({ success: true, message: 'Email enviado com sucesso!' });
     } catch (error) {
         console.error('Erro ao enviar o email:', error);
@@ -56,7 +44,7 @@ app.post('/api/contact', async (req, res) => {
     }
 });
 
-// Inicia o servidor
+app.options('api/contact', cors());
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
 });
